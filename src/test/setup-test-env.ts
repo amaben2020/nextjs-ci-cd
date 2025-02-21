5a1bc9dfcbe76ae9a499a5beba97c83f46e57ed9
amaben2020 <60056927+amaben2020@users.noreply.github.com>
2025-02-21 19:08:10 +0100

    feat: sas
    BREAKING CHANGE: sas

---
+++ src/test/setup-test-env.ts
@@ -0,0 +1,7 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+import '@testing-library/jest-dom/vitest';
+
+// See <https://reactjs.org/blog/2022/03/08/react-18-upgrade-guide.html#configuring-your-testing-environment>.
+
+// @ts-ignore
+globalThis.IS_REACT_ACT_ENVIRONMENT = true;
